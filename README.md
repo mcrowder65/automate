244fe580ef942a5e1afaae5ec9f5a86c69397550
Matt Crowder <mcrowder65@gmail.com>
2017-11-18 16:12:39 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## MATT'S LIFE AUTOMATION PROJECT
+
+
+This is an app where I am going to combine most apps I use into one.
+
+Weight tracking (my fitness pal)
+
+Working out (strong lifts, which i have abandoned for the simple notes app but I want data for machine learning)
+
+Budgeting (I just use a google spreadsheet, but I want to be able to add things easily, and it will still just be added into google sheets)
+
+My task manager (which is just an emailer)
+
+
+Bringing these all together will be amazing and by the time i do i will probably have to go back and 
+migrate them all to newer tech :(
---
+++ src/client/app.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {createStore, applyMiddleware, compose} from "redux";
+import {Provider} from "react-redux";
+import "babel-polyfill";
+import createSagaMiddleware from "redux-saga";
+import sagaActions from "./actions/sagas/index";
+
+import rootReducer from "./reducers/index";
+import Router from "./router";
+import initialState from "./reducers/initial-state";
+import "./styles/base.css";
+
+const sagaMiddleware = createSagaMiddleware();
+/* eslint no-underscore-dangle: "off" */
+/* global window */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, initialState,
+    composeEnhancers(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(sagaActions);
+const App = () => (
+    <Provider store={store}>
+        <Router/>
+    </Provider>
+);
+
+/* global document */
+ReactDOM.render(<App/>, document.getElementById("root"));
